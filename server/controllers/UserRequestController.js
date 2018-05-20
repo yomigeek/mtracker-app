c2814c4eba9eafe0a0a470a8841b1533f1f958d0
Yomi Olaoye <seayomi@gmail.com>
2018-05-20 15:50:14 +0100

    ft(user-create-request-dummy-api): Implement a create request dummy API
    - Create an API endpoint that enable user to create a request using
    dummy data
    
    - Develop the endpoint using the Nodejs/Express framework
    
    [finishes #157650761]

--- server/controllers/UserRequestController.js
+++ server/controllers/UserRequestController.js
@@ -9,6 +9,28 @@ class UserRequestController {
       },
     });
   }
+
+
+  static createRequest(req, res) {
+    const newRequest = ({
+      id: requests.length + 1,
+      title: req.body.title,
+      description: req.body.description,
+      date: '2018-18-05',
+      status: 'open',
+      priority: req.body.priority,
+      userId: 111,
+    });
+    requests.push(newRequest);
+    return res.status(201).json({
+
+      status: 'success',
+      message: 'Request created successfully!',
+      data: {
+        requests: newRequest,
+      },
+    });
+  }
 }
 
 export default UserRequestController;
