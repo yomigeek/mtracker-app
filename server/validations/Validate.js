9524b9acc3ee7ba0f8887990506a3f4921bd6387
Yomi Olaoye <seayomi@gmail.com>
2018-05-23 16:24:47 +0100

    ft(api-user-signup): Implement the User Signup API
    - Implement the TDD on ther user signup api endpoint
    
    - Implement User signup API endpoint to that user can signup with
    required credentials
    
    [finishes #157753167]

--- server/validations/Validate.js
+++ server/validations/Validate.js
@@ -3,7 +3,7 @@ class Validate {
     const titleRequest = req.body.title;
     const descriptionRequest = req.body.description;
     const priorityRequest = req.body.priority;
-    
+
     if ((!titleRequest) || (!descriptionRequest) || (!priorityRequest)) {
       return res.status(500).json({
         status: 'fail',
@@ -64,6 +64,93 @@ class Validate {
         message: 'Priority must be either low, medium or high',
       });
     } next();
+  }
+
+  static signUpValidate(req, res, next) {
+    const fullname = req.body.fullname;
+    const email = req.body.email;
+    const department = req.body.department;
+    const password = req.body.password;
+    const trimedFullname = fullname.replace(/^\s+|\s+$|\s+(?=\s)/g, '');
+    const trimedEmail = email.replace(/^\s+|\s+$|\s+(?=\s)/g, '');
+    const trimedDepartment = department.replace(/^\s+|\s+$|\s+(?=\s)/g, '');
+    const trimedPassword = password.replace(/^\s+|\s+$|\s+(?=\s)/g, '');
+    const regularExpressionEmail = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    const regularExpressionOthers = /^[a-zA-Z0-9- ]*$/;
+
+    if (!fullname) {
+      return res.status(400).json({
+        status: 'fail',
+        message: 'fullname cannot be empty!',
+      });
+    } else if (fullname.length < 2) {
+      return res.status(400).json({
+        status: 'fail',
+        message: 'fullname must be more than 2 characters',
+      });
+    } else if (!regularExpressionOthers.test(fullname)) {
+      return res.status(400).json({
+        status: 'fail',
+        message: 'Wrong fullname format! Can only contain spaces, hypens as special characters',
+      });
+    } else if (trimedFullname.length <= 0) {
+      return res.status(400).json({
+        status: 'fail',
+        message: 'Fullname cannot be empty',
+      });
+    } else if (!email) {
+      return res.status(400).json({
+        status: 'fail',
+        message: 'email cannot be empty!',
+      });
+    } else if (!regularExpressionEmail.test(email)) {
+      return res.status(400).json({
+        status: 'fail',
+        message: 'Wrong email format!',
+      });
+    } else if (trimedEmail.length <= 0) {
+      return res.status(400).json({
+        status: 'fail',
+        message: 'Email cannot be empty!',
+      });
+    } else if (!department) {
+      return res.status(400).json({
+        status: 'fail',
+        message: 'department cannot be empty!',
+      });
+    } else if (department.length < 2) {
+      return res.status(400).json({
+        status: 'fail',
+        message: 'department name must be 2 or more characters',
+      });
+    } else if (!regularExpressionOthers.test(department)) {
+      return res.status(400).json({
+        status: 'fail',
+        message: 'Wrong department name format! Can only contain spaces, hypens as special characters',
+      });
+    } else if (trimedDepartment.length <= 0) {
+      return res.status(400).json({
+        status: 'fail',
+        message: 'Department cannot be empty!',
+      });
+    } else if (!password) {
+      return res.status(400).json({
+        status: 'fail',
+        message: 'Password cannot be empty!',
+      });
+    } else if (password.length < 5) {
+      return res.status(400).json({
+        status: 'fail',
+        message: 'Password must be 5 characters and above',
+      });
+    } else if (trimedPassword.length <= 0) {
+      return res.status(400).json({
+        status: 'fail',
+        message: 'Password cannot be empty!',
+      });
+    }
+
+    next();
   }
 }
 
