2cfaa36f6b713206fce876be74afaa1a21fe7c61
Yomi Olaoye <seayomi@gmail.com>
2018-05-28 12:26:40 +0100

    ft(user-single-request): User Fetch A Request Detail API
    - Develop an API that enables a user to fetch a request details
    
    - Implement TDD while developing the API endpoint
    
    [finishes #157754212]

--- server/test/userRequests.js
+++ server/test/userRequests.js
@@ -40,6 +40,22 @@ describe('User request API Tests', () => {
       });
   });
 
+  it('should fail on fetch a user request detail on non-existing request on /api/v1/users/requests GET', (done) => {
+    chai.request(app)
+      .get(`/api/v1/users/requests/${10}`)
+      .set('x-access-token', token)
+      .end((err, res) => {
+        res.should.have.status(404);
+        res.should.be.json;
+        res.body.should.be.a('object');
+        res.body.should.have.property('status');
+        res.body.should.have.property('message');
+        res.body.status.should.equal('fail');
+
+        done();
+      });
+  });
+
   it('should add a SINGLE request on /api/v1/users/requests POST', (done) => {
     chai.request(app)
       .post('/api/v1/users/requests')
@@ -162,6 +178,22 @@ describe('User request API Tests', () => {
   it('should fetch ALL user requests on /api/v1/users/requests GET', (done) => {
     chai.request(app)
       .get('/api/v1/users/requests')
+      .set('x-access-token', token)
+      .end((err, res) => {
+        res.should.have.status(200);
+        res.should.be.json;
+        res.body.should.be.a('object');
+        res.body.should.have.property('status');
+        res.body.status.should.equal('success');
+        res.body.data.should.have.property('requests');
+
+        done();
+      });
+  });
+
+  it('should fetch a user request detail on /api/v1/users/requests GET', (done) => {
+    chai.request(app)
+      .get(`/api/v1/users/requests/${1}`)
       .set('x-access-token', token)
       .end((err, res) => {
         res.should.have.status(200);
