56a9404e88148e4c9c85bc35d0d24e942941b80b
Yomi Olaoye <seayomi@gmail.com>
2018-05-31 00:43:18 +0100

    ft(user-signup-ui-api): Implement User Signup page with API
    - Implement the user signup page with the API endpoint
    
    - Ensure API endpoint is called when create account button is clicked
    
    - Ensure the api returns the right response
    
    [finishes #157987282]

--- public/assets/js/fetch.js
+++ public/assets/js/fetch.js
@@ -1,4 +1,6 @@
 document.getElementById('postForm').addEventListener('submit', userLogin);
+document.getElementById('signup-form').addEventListener('submit', userSignup);
+
 const baseUrl = 'https://mtrack-app.herokuapp.com';
 
 function userLogin(event) {
@@ -42,6 +44,48 @@ function userLogin(event) {
         document.querySelector('.error').style.display = 'block';
         document.querySelector('.working-msg').style.display = 'none';
 
+      }
+    }
+    )
+    .catch((err) => console.log(err))
+}
+
+
+
+function userSignup(event) {
+
+  document.querySelector('#signup-process').style.display = 'block';
+  document.querySelector('#signup-form').style.display = 'none';
+
+  event.preventDefault();
+
+  let userEmail = document.getElementById('email').value;
+  let userPassword = document.getElementById('password').value;
+  let userDepartment = document.getElementById('department').value;
+  let userFullname = document.getElementById('fullname').value;
+
+  fetch(baseUrl + '/api/v1/auth/signup', {
+    method: 'POST',
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({ email: userEmail, password: userPassword, fullname: userFullname, department: userDepartment })
+  }).then((response) => { return response.json() })
+    .then((data) => {
+      console.log(data);
+      if (data.status == 'success') {
+
+
+        document.querySelector('#signup-process').style.display = 'none';
+        document.querySelector('.working-msg').style.display = 'block';
+        document.querySelector('#signup-form').style.display = 'none';
+
+      } else {
+
+        document.querySelector('.error').style.display = 'block';
+        document.querySelector('.working-msg').style.display = 'none';
+        document.querySelector('#signup-form').style.display = 'block';
+        document.querySelector('.error').innerHTML = data.message;
       }
     }
     )
