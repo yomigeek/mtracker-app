2223b2d5275c98e4a99d5612f2e473c5fb5ab8f9
Yomi Olaoye <seayomi@gmail.com>
2018-06-25 09:04:05 +0100

    bg(code-review-error-msg): Implement Code Review Feedback
    - Correct the error messages context and display experience based on code reviewfeedback
    
    [finishes #158485403]

--- server/controllers/UserLoginController.js
+++ server/controllers/UserLoginController.js
@@ -11,7 +11,7 @@ class UserLoginController {
         return res.status(400).json({
 
           status: 'fail',
-          message: 'Invalid Email!',
+          message: 'Invalid Login details!',
         });
       }
       const comparedpassword = bcrypt
